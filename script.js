0fa5305cf2c9f5599fa8c8ebb8df1f572d1a0803
Samuel Key <s.key08@gmail.com>
2015-11-17 14:00:30 -0500

    organizing files

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+  $("#one").click(function() {
+    $(this).find('span').toggleClass("glyphicon-chevron-up");
+    $(this).find('span').toggleClass("glyphicon-chevron-down");
+    $("#pone").toggle();
+  })
+
+  $("#two").click(function() {
+    $(this).find('span').toggleClass("glyphicon-chevron-up");
+    $(this).find('span').toggleClass("glyphicon-chevron-down");
+    $("#ptwo").toggle();
+  })
+
+  $("#three").click(function() {
+    $(this).find('span').toggleClass("glyphicon-chevron-up");
+    $(this).find('span').toggleClass("glyphicon-chevron-down");
+    $("#pthree").toggle();
+  })
+
 });
